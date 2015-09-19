2d2cde667ef9dc7456aca81c2be1e4a80b625d7b
Chetana Vedantam <chetana.vedantam@gmail.com>
2015-09-19 17:34:19 -0500

    Added main

---
+++ src/MS_GSP.java
@@ -0,0 +1,19 @@
+/**
+ * This is the main class
+ */
+
+/**
+ * @author vedantam
+ *
+ */
+public class MS_GSP {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// main algorithm goes here
+
+	}
+
+}
