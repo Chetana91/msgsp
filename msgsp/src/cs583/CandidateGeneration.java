37c0d638c10a5ddc5891abb486c2461d4460ceb1
Chetana Vedantam <chetana.vedantam@gmail.com>
2015-09-26 12:01:48 -0500

    fixed merge conflicts

--- msgsp/src/cs583/CandidateGeneration.java
+++ msgsp/src/cs583/CandidateGeneration.java
@@ -1,9 +1,11 @@
 package cs583;
 
 import java.util.ArrayList;
+import java.math.*;
+
 
 public class CandidateGeneration {
-
+	
 	/**
 	 * @param L
 	 * @return
@@ -22,14 +24,61 @@ public class CandidateGeneration {
 		f1.printFrequentSequence('F', 1);
 		return f1;
 	}
-
-	public FrequentSequence level2CandidateGen(ArrayList<Integer> L) {
-		// function takes argument L and returns a superset of the set of all frequent 2 itemsets
-		FrequentSequence C2 = new FrequentSequence();
-		for (Integer i : L) {
-
-		}
-
+	
+	public FrequentSequence level2CandidateGen(ArrayList<Integer> L)
+	{ //function takes argument L and returns a superset of the set of all frequent 2 itemsets
+	   FrequentSequence C2= new FrequentSequence();
+	   for(Integer i: L)
+	   {  if(MsGsp.ItemCountMap.get(L.get(i))*1.0/ MsGsp.N >= MsGsp.MISMap.get(L.get(i)))
+	   			{
+		   			for(int h=i; h < L.size();h++){
+		   				if(MsGsp.ItemCountMap.get(L.get(h))*1.0 / MsGsp.N >= MsGsp.MISMap.get(L.get(i)))
+		   				{
+		   					if (Math.abs(MsGsp.ItemCountMap.get(L.get(i)).intValue() - MsGsp.ItemCountMap.get(L.get(h)).intValue()) <= MsGsp.SDC * MsGsp.N)
+		   					{
+		   						ItemSet itemset = new ItemSet();
+		   						int a= L.get(i);
+		   						int b=L.get(h);
+		   						
+		   						//adding sequence<{a},{b}>
+		   						ItemSet firstset= new ItemSet();
+		   						firstset.items.add(a);
+		   						ItemSet secondset= new ItemSet();
+		   						secondset.items.add(b);
+		   						DataSequence tempsequence1= new DataSequence();
+		   						tempsequence1.sequence.add(firstset);
+		   						tempsequence1.sequence.add(secondset);
+		   						C2.addIntermediateSequence(tempsequence1); //tempsequence1 is <{a},{b}>
+		   						
+		   						
+		   						
+		   						if(a<=b)
+		   						{
+		   							itemset.items.add(a);
+		   							itemset.items.add(b);
+		   						}
+		   						else
+		   						{
+		   							itemset.items.add(b);
+		   							itemset.items.add(a);
+		   						}
+		   						if(a!=b)
+		   						{
+		   							DataSequence tempsequence2= new DataSequence();
+		   							tempsequence2.sequence.add(itemset); //adds tempsequence2 <{a,b}> 
+		   							C2.addIntermediateSequence(tempsequence2);
+		   						}
+		   						
+		   						
+		   						
+		   						
+		   					}
+		   				}
+		   			}
+		   				   			
+		   	}
+		   			
+	   }
 		return C2;
 	}
 	
