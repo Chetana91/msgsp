e1166637cc41e72c705e05b48a48d39f82bca216
garimakriplani <garimakriplani@gmail.com>
2015-09-25 20:39:59 -0500

    Added  class- FrequentSequence

---
+++ msgsp/src/cs583/CandidateGeneration.java
@@ -0,0 +1,17 @@
+package cs583;
+
+import java.util.ArrayList;
+
+public class CandidateGeneration {
+	public FrequentSequence level2CandidateGen(ArrayList<Integer> L)
+	{ //function takes argument L and returns a superset of the set of all frequent 2 itemsets
+	   FrequentSequence C2= new FrequentSequence();
+	   for(Integer i: L)
+	   { 
+		   
+	   }
+		
+		return C2;
+	}
+
+}
---
+++ msgsp/src/cs583/FrequentSequence.java
@@ -0,0 +1,25 @@
+package cs583;
+
+import java.util.HashSet;
+
+public class FrequentSequence {
+	public HashSet<DataSequence> sequence= new HashSet<DataSequence>();
+	
+	public FrequentSequence() {
+		// TODO Auto-generated constructor stub
+		//default constructor to initialize to an empty frequentsequence
+		sequence= new HashSet<DataSequence>();
+	}
+	public FrequentSequence(HashSet<DataSequence> paramsequence)
+	{
+		sequence.addAll(paramsequence);
+	}
+	public void addIntermediateSequence(DataSequence seq)
+	{  //this function is used to append a sequence to the existing result sequence when forming candidates
+		sequence.add(seq);
+	}
+	public void addFrequentSequence(FrequentSequence fs)
+	{  //this sequence adds all the sequences of a frequent sequence,formed in previous step
+		sequence.addAll(fs.sequence);
+	}
+}
